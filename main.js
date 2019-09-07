a4a2a080df775146b91bb0fc2a0e0ff5745ce59a
Piotr Bąbel <ludwik.babel@gmail.com>
2019-09-07 13:45:53 +0200

    BEM

--- main.js
+++ main.js
@@ -3,3 +3,18 @@ const name = 'Ludwik';
 const age = 28;
 console.log(name);
 console.log(age);
+
+console.log(`Nazywam się ${name} i mam lat ${age} lat`);
+
+console.log('Witaj przybyszu');
+
+const about = document.querySelector('.about__paragraph--js');
+
+console.log(about.innerHTML);
+
+about.innerHTML = `<strong>Nazywam</strong> się ${name} i mam lat ${age} lat`;
+
+const paragraphs = document.querySelectorAll('p');
+
+console.log(paragraphs);
+console.log(paragraphs[0]);
